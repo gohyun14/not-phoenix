ac7da8ed91c087f7bc7a132596a436f392c7a974
Gabe Hyun <gabe.hyun@gmail.com>
2024-04-07 19:08:48 -0400

    added market order form

--- src/components/PlaceOrder/PlaceOrder.tsx
+++ src/components/PlaceOrder/PlaceOrder.tsx
@@ -2,6 +2,8 @@
 
 import Image from 'next/image';
 import { useState } from 'react';
+import { InformationCircleIcon } from '@heroicons/react/24/solid';
+import * as Tooltip from '@radix-ui/react-tooltip';
 
 export default function PlaceOrder() {
   const [orderType, setOrderType] = useState<'limit' | 'market'>('limit');
@@ -75,13 +77,22 @@ export default function PlaceOrder() {
         </div>
 
         {/* order form */}
-        {orderType === 'limit' ? <LimitOrderForm /> : <MarketOrderForm />}
+        {orderType === 'limit' ? (
+          <LimitOrderForm isBuy={orderAction === 'buy'} />
+        ) : (
+          <MarketOrderForm isBuy={orderAction === 'buy'} />
+        )}
       </div>
     </div>
   );
 }
 
-function LimitOrderForm() {
+const USDCImgSrc =
+  'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png';
+const SOLImgSrc =
+  'https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png';
+
+function LimitOrderForm({ isBuy = true }: { isBuy?: boolean }) {
   return (
     <div>
       <p className="pt-[20px] text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]">
@@ -96,9 +107,11 @@ function LimitOrderForm() {
           className="w-full rounded-[16px] bg-black/5 py-[12px] pl-[45px] pr-[25px] text-[18px] font-[400] leading-[100%] tracking-[0.05rem] text-[#575151] focus:outline-none focus:ring-2 focus:ring-orange-400"
         />
 
-        <img
-          src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png"
+        <Image
+          src={USDCImgSrc}
           alt="USDC"
+          height={25}
+          width={25}
           className="absolute left-[10px] top-1/2 h-[25px] w-[25px] translate-y-[-50%] transform rounded-full object-cover"
         />
       </div>
@@ -118,9 +131,11 @@ function LimitOrderForm() {
           className="w-full rounded-[16px] bg-black/5 py-[12px] pl-[45px] pr-[25px] text-[18px] font-[400] leading-[100%] tracking-[0.05rem] text-[#575151] focus:outline-none focus:ring-2 focus:ring-orange-400"
         />
 
-        <img
-          src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v/logo.png"
-          alt="USDC"
+        <Image
+          src={isBuy ? USDCImgSrc : SOLImgSrc}
+          alt={isBuy ? 'USDC' : 'SOL'}
+          height={25}
+          width={25}
           className="absolute left-[10px] top-1/2 h-[25px] w-[25px] translate-y-[-50%] transform rounded-full object-cover"
         />
       </div>
@@ -137,26 +152,176 @@ function LimitOrderForm() {
           className="w-full rounded-[16px] bg-black/5 py-[12px] pl-[45px] pr-[25px] text-[18px] font-[400] leading-[100%] tracking-[0.05rem] text-[#575151] focus:outline-none focus:ring-2 focus:ring-orange-400"
         />
 
-        <img
-          src="https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/So11111111111111111111111111111111111111112/logo.png"
-          alt="SOL"
+        <Image
+          src={!isBuy ? USDCImgSrc : SOLImgSrc}
+          alt={!isBuy ? 'USDC' : 'SOL'}
+          height={25}
+          width={25}
           className="absolute left-[10px] top-1/2 h-[25px] w-[25px] translate-y-[-50%] transform rounded-full object-cover"
         />
       </div>
+
+      <div className="mt-[20px] flex w-full flex-row items-center justify-between">
+        <div className="flex w-full flex-row items-center gap-[3px]">
+          <p className="text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]/60">
+            Est. fee
+          </p>
+          <Tooltip.Provider delayDuration={0}>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <InformationCircleIcon className="h-[13px] w-[13px] shrink-0 fill-[#575151]/60" />
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  className="flex flex-col gap-y-[12px] rounded-[8px] bg-[#f1f1f1] px-[15px] py-[10px] text-[15px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]"
+                  side="top"
+                  sideOffset={8}
+                >
+                  <p className="max-w-[150px] text-[11.5px] font-[500] leading-[14px] tracking-[0.05em] text-[#575151]">
+                    SOL/USDC fees are 0.02% for taker orders and 0% for maker
+                    orders
+                  </p>
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        </div>
+
+        <div className="flex shrink-0 flex-row items-center justify-end gap-[7px]">
+          <p className="text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]/60">
+            ---
+          </p>
+          <Image
+            src={USDCImgSrc}
+            alt="USDC"
+            height={25}
+            width={25}
+            className=" h-[25px] w-[25px] rounded-full object-cover"
+          />
+        </div>
+      </div>
+
+      <button className="my-[20px] w-full rounded-full bg-orange-400 py-[10px] text-[16px] font-[500] uppercase tracking-[0.04em] text-white transition-transform duration-100 ease-in-out hover:scale-[1.02] active:scale-100">
+        Place Order
+      </button>
     </div>
   );
 }
 
-function MarketOrderForm() {
+function MarketOrderForm({ isBuy = true }: { isBuy?: boolean }) {
   return (
     <div>
-      <div>
-        <label htmlFor="limit-price">Price</label>
-        <input type="number" id="limit-price" />
+      <p className="pt-[20px] text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]">
+        Size
+      </p>
+      <p className="mt-[10px] text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]/60">
+        Pay
+      </p>
+      <div className="relative my-[5px] w-full">
+        <input
+          inputMode="decimal"
+          placeholder="0.00"
+          spellCheck="false"
+          pattern="^[0-9]*[.,]?[0-9]*$"
+          className="w-full rounded-[16px] bg-black/5 py-[12px] pl-[45px] pr-[25px] text-[18px] font-[400] leading-[100%] tracking-[0.05rem] text-[#575151] focus:outline-none focus:ring-2 focus:ring-orange-400"
+        />
+
+        <Image
+          src={isBuy ? USDCImgSrc : SOLImgSrc}
+          alt={isBuy ? 'USDC' : 'SOL'}
+          height={25}
+          width={25}
+          className="absolute left-[10px] top-1/2 h-[25px] w-[25px] translate-y-[-50%] transform rounded-full object-cover"
+        />
       </div>
-      <div>
-        <label htmlFor="limit-quantity">Quantity</label>
-        <input type="number" id="limit-quantity" />
+
+      <p className="mt-[15px] text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]/60">
+        Receive
+      </p>
+      <div className="relative my-[5px] w-full">
+        <input
+          inputMode="decimal"
+          placeholder="0.00"
+          spellCheck="false"
+          pattern="^[0-9]*[.,]?[0-9]*$"
+          className="w-full rounded-[16px] bg-black/5 py-[12px] pl-[45px] pr-[25px] text-[18px] font-[400] leading-[100%] tracking-[0.05rem] text-[#575151] focus:outline-none focus:ring-2 focus:ring-orange-400"
+        />
+
+        <Image
+          src={!isBuy ? USDCImgSrc : SOLImgSrc}
+          alt={!isBuy ? 'USDC' : 'SOL'}
+          height={25}
+          width={25}
+          className="absolute left-[10px] top-1/2 h-[25px] w-[25px] translate-y-[-50%] transform rounded-full object-cover"
+        />
+      </div>
+
+      <button className="my-[20px] w-full rounded-full bg-orange-400 py-[10px] text-[16px] font-[500] uppercase tracking-[0.04em] text-white transition-transform duration-100 ease-in-out hover:scale-[1.02] active:scale-100">
+        Place Order
+      </button>
+
+      <p className="mb-[8px] mt-[15px] text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]">
+        Summary
+      </p>
+      <div className="h-[1px] w-full bg-[#575151]" />
+
+      <div className="mt-[10px] flex w-full flex-row items-center justify-between">
+        <p className="text-[14px] font-[400] leading-[16px] text-[#575151]/90">
+          Est. Average Price
+        </p>
+
+        <div className="flex shrink-0 flex-row items-center justify-end gap-[7px]">
+          <p className="text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]/60">
+            ---
+          </p>
+          <Image
+            src={USDCImgSrc}
+            alt="USDC"
+            height={25}
+            width={25}
+            className=" h-[25px] w-[25px] rounded-full object-cover"
+          />
+        </div>
+      </div>
+
+      <div className="mt-[10px] flex w-full flex-row items-center justify-between">
+        <div className="flex w-full flex-row items-center gap-[3px]">
+          <p className="text-[14px] font-[400] leading-[16px] text-[#575151]/90">
+            Est. Fee
+          </p>
+          <Tooltip.Provider delayDuration={0}>
+            <Tooltip.Root>
+              <Tooltip.Trigger asChild>
+                <InformationCircleIcon className="h-[13px] w-[13px] shrink-0 fill-[#575151]/60" />
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <Tooltip.Content
+                  className="flex flex-col gap-y-[12px] rounded-[8px] bg-[#f1f1f1] px-[15px] py-[10px] text-[15px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px]"
+                  side="top"
+                  sideOffset={8}
+                >
+                  <p className="max-w-[150px] text-[11.5px] font-[500] leading-[14px] tracking-[0.05em] text-[#575151]">
+                    SOL/USDC fees are 0.02% for taker orders and 0% for maker
+                    orders
+                  </p>
+                </Tooltip.Content>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        </div>
+
+        <div className="flex shrink-0 flex-row items-center justify-end gap-[7px]">
+          <p className="text-[11.5px] font-[500] uppercase leading-[12px] tracking-[0.05em] text-[#575151]/60">
+            ---
+          </p>
+          <Image
+            src={USDCImgSrc}
+            alt="USDC"
+            height={25}
+            width={25}
+            className=" h-[25px] w-[25px] rounded-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
